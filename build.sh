8c9bf6180f5ffcca511a503485d618c32681ebd4
ibuetler <ivan.buetler@hacking-lab.com>
2023-10-31 23:55:36 +0100

    init

--- build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker build --no-cache -t hackinglab/alpine-terraform-websocketd:3.2.0 -t hackinglab/alpine-terraform-websocketd:3.2 -t hackinglab/alpine-terraform-websocketd:latest -f Dockerfile .
